cf52bd95078738205d152e805d2273d361f9f392
Nathaniel Rankin <rankinna@bc.edu>
2020-12-30 17:38:27 -0600

    Img fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "../styles.css";
 
+import Facebook from "/img/icons/facebook.png";
+import Instagram from "/img/icons/instagram.png";
+import LinkedIn from "/img/icons/linkedin.png";
+import GitHub from "/img/icons/github.png";
+
 class Footer extends React.Component {
   render() {
     return (
@@ -17,16 +22,16 @@ class Footer extends React.Component {
             <img src="/img/icons/mail.png" />
           </a>
           <a href="https://www.facebook.com/nathan.rankin.908" target="_blank">
-            <img src="/img/icons/facebook.png" />
+            <img src={Facebook} />
           </a>
           <a href="https://www.instagram.com/rankin_n/" target="_blank">
-            <img src="/img/icons/instagram.png" />
+            <img src={Instagram} />
           </a>
           <a href="https://www.linkedin.com/in/nathanrankin/" target="_blank">
-            <img src="/img/icons/linkedin.png" />
+            <img src={LinkedIn} />
           </a>
           <a href="https://github.com/nrankin18" target="_blank">
-            <img src="/img/icons/github.png" />
+            <img src={GitHub} />
           </a>
         </span>
       </div>
